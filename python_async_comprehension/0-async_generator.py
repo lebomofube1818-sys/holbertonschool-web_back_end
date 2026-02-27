091e070e7fa86f77fc94983be2d3bfe152b2ec46
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-02-27 16:41:43 +0200

    0. Async generator coroutine and test main file

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,12 @@
 import asyncio
 import random
 
+
 async def async_generator():
-    """Coroutine that yields 10 random numbers between 0 and 10, waiting 1 second between each."""
+    """
+    Coroutine that yields 10 random numbers between 0 and 10,
+    waiting 1 second between each.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
