06037c5d517818738e22caae7ec3a2b193ca8e8e
lebomofube1818-sys <lebomofube1818-sys@gmail.com>
2026-03-27 13:03:42 +0200

    Fix final PEP8 line length issues for deletion-resilient pagination

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i]
+                for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """Return a deletion-resilient page of the dataset"""
+        dataset_indexed = self.indexed_dataset()
+        assert 0 <= index < len(dataset_indexed), "Index out of range"
+
+        data = []
+        current_index = index
+        collected = 0
+
+        # proper PEP8-compliant while loop
+        while (collected < page_size
+               and current_index < len(dataset_indexed)):
+            if current_index in dataset_indexed:
+                data.append(dataset_indexed[current_index])
+                collected += 1
+            current_index += 1
+
+        next_index = current_index if current_index < len(dataset_indexed) else None
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data
+        }
